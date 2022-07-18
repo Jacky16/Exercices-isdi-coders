729775b89d21742b2b2196c59da9fecde5dd78dd
Adria <adriamartinez94@gmail.com>
2022-07-18 12:31:36 +0200

    Hecha la calculadora pro

--- Calc_JS/main.js
+++ Calc_JS/main.js
@@ -1,80 +1,98 @@
 let results = [];
-const inputUser = [];
+let inputsUser = [];
 
-function calc() {
-  let numberOne = prompt("Ingrese el primer número");
-  let numberTwo = prompt("Ingrese el segundo número");
-  if (!isNaN(numberOne) && numberTwo == "") {
-    //Calcular raiz cuadrada
-    console.log(
-      "La raiz cuadrada de " +
-        numberOne +
-        " es: " +
-        Math.sqrt(numberOne).toFixed(3)
-    );
-  } else if (isNaN(numberOne) || isNaN(numberTwo)) {
-    console.log("Ingrese numeros validos");
-  } else {
-    numberOne = parseFloat(numberOne);
-    numberTwo = parseFloat(numberTwo);
-    sum(numberOne, numberTwo);
-    sub(numberOne, numberTwo);
-    mul(numberOne, numberTwo);
-    div(numberOne, numberTwo);
+function sum() {
+  let acc = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    acc += arguments[i];
+  }
+  return acc;
+}
+function sub() {
+  let acc = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    acc -= arguments[i];
+  }
+  return acc;
+}
+function mul() {
+  let acc = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    acc *= arguments[i];
+  }
+  return acc;
+}
+function div() {
+  let acc = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    acc /= arguments[i];
+  }
+  return acc;
+}
+
+const showResults = () => {
+  console.clear();
+  results.forEach((result) => {
+    console.log(`Operación: ${results.indexOf(result) + 1}`);
+    console.log("---------------------------------");
+    if (result.sum != undefined)
+      console.log(`El resultado de la suma es: ${result.sum}`);
+    if (result.sub != undefined)
+      console.log(`El resultado de la resta es: ${result.sub}`);
+    if (result.mul != undefined)
+      console.log(`El resultado de la multiplicación es: ${result.mul}`);
+    if (result.div != undefined)
+      console.log(`El resultado de la división es: ${result.div}`);
+    if (result.sqrt != undefined)
+      console.log(`El resultado de la raiz cuadrada es: ${result.sqrt}`);
+    console.log("---------------------------------");
+  });
+};
+const askUserNumbers = () => {
+  const inputNumber = prompt("Ingrese un número");
+
+  if (Number(inputNumber) != isNaN && inputNumber != "") {
+    inputsUser.push(Number(inputNumber));
+    askUserNumbers();
+  } else if (inputNumber == "") {
+    calculate();
+  }
+};
+const askAgain = () => {
+  const input = prompt("¿Desea calcular otro número? (s/n)");
+  if (input === "s") {
+    inputsUser = [];
+    askUserNumbers();
+  } else if ((input === "n") | (input == null)) {
+    console.log("Hasta luego");
+    return;
+  } else if ((input == "") | (Number(input) == isNaN)) {
+    alert("Ingrese una opción válida");
+    askAgain();
+  }
+};
+const calculate = () => {
+  const sizeInputs = inputsUser.length;
+  if (sizeInputs === 1) {
+    const sqrtResult = Math.sqrt(inputsUser[0]);
+    const result = { sqrt: sqrtResult };
+    results.push(result);
     showResults();
+  } else if (sizeInputs > 1) {
+    const result = {
+      sum: sum(...inputsUser),
+      sub: sub(...inputsUser),
+      mul: mul(...inputsUser),
+      div: div(...inputsUser),
+    };
+    results.push(result);
   }
-}
+  showResults();
 
-function sum(a, b) {
-  let acc = 0;
-  for (num in arguments) {
-    acc += arguments[num];
-  }
-  results.push(a + b);
-}
-function sub(a, b) {
-  let acc = 0;
-  for (num in arguments) {
-    acc = acc - arguments[parseInt(num)];
-  }
-  results.push(a - b);
-}
-function mul(a, b) {
-  let acc = 1;
-  for (num in arguments) {
-    acc *= arguments[num];
-  }
-  results.push(a * b);
-}
-function div(a, b) {
-  let acc = 1;
-  for (num in arguments) {
-    acc /= arguments[num];
-  }
-  results.push(a / b);
-}
-function showResults() {
-  console.log(results);
-  let countResults = 0;
-  for (num in results) {
-    if (countResults === 5) {
-      console.log("---------------------------");
-      countResults = 0;
-    } else {
-      if (num == 0) {
-        console.log("La suma es: " + results[num]);
-      } else if (num == 1) {
-        console.log("La resta es: " + results[num]);
-      } else if (num == 2) {
-        console.log("La multiplicación es: " + results[num]);
-      } else if (num == 3) {
-        console.log("La división es: " + results[num]);
-      } else if (num == 4) {
-        console.log("La raiz cuadrada es: " + results[num]);
-      }
-      countResults++;
-    }
-  }
-}
+  askAgain();
+};
 
-calc();
+const main = () => {
+  askUserNumbers();
+};
+main();
